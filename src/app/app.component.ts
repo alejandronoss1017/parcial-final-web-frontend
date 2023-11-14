5f572ccf1bb7dd9ec7ed31fb4f0c77c5f75bf42b
Camilo Nossa <calejandro_noss@outlook.com>
2023-11-14 17:55:25 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'final-frontend-web';
+  stringsList: string[] = ['one', 'two', 'three'];
+
+  bookForm = new FormGroup({
+    name: new FormControl(''),
+    library: new FormControl(''),
+  });
+
+  onSubmit() {
+    console.log('submit');
+  }
 }
